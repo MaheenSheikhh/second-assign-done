01f6ce205a2a9b37668f6b01f6a04cf550720164
MaheenSheikh <maheensheikh865@gmail.com>
2024-04-28 19:03:13 +0500

    first commit

---
+++ assign.js
@@ -0,0 +1,3 @@
+var personname = "eric,";
+var message = "Hello! ".concat(personname, " would you like to learn some python today?");
+console.log(message);
---
+++ assign.ts
@@ -0,0 +1,6 @@
+
+ let personname: string= "eric,"
+
+let message = `Hello! ${personname} would you like to learn some python today?`
+
+console.log(message);
